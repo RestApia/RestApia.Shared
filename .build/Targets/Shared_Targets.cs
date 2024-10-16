dcde4d474af66de72f5490526fe49b533ec9caf3
Nick Rimmer <NickRimmer@users.noreply.github.com>
2024-10-16 22:32:46 +0200

    build and push shared lib

---
+++ .build/Targets/Shared_Targets.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using NuGet.Common;
+using NuGet.Protocol;
+using NuGet.Protocol.Core.Types;
+using NuGet.Versioning;
+using Nuke.Common;
+using Nuke.Common.Tools.DotNet;
+using Serilog;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+[SuppressMessage("ReSharper", "CheckNamespace")]
+[SuppressMessage("ReSharper", "AllUnderscoreLocalParameterName")]
+[SuppressMessage("ReSharper", "UnusedMember.Local")]
+partial class Build
+{
+    [Parameter("Shared library version")]
+    string SharedLibVersion = EnvironmentInfo.GetVariable<string>("SHARED_LIB_VERSION") ?? string.Empty;
+
+    string PushApiKey => EnvironmentInfo.GetVariable<string>("NUGET_API") ?? Settings.Value<string>("nuget_push_api_key");
+
+    Target Shared_FindNextVersion => _ => _
+        .OnlyWhenDynamic(() => string.IsNullOrWhiteSpace(SharedLibVersion))
+        .Executes(async () =>
+        {
+            // find nuget with name "RestApia.Shared" and get next possible version
+            var repository = Repository.Factory.GetCoreV3("https://api.nuget.org/v3/index.json");
+            var resources = await repository.GetResourceAsync<FindPackageByIdResource>();
+            var metadata = await resources.GetAllVersionsAsync("RestApia.Shared", new SourceCacheContext(), NullLogger.Instance, CancellationToken.None);
+            var currentVersion = metadata
+                .OrderByDescending(x => x)
+                .FirstOrDefault() ?? new NuGetVersion("0.1.0");
+
+            SharedLibVersion = currentVersion.Version.Major + "." + currentVersion.Version.Minor + "." + (currentVersion.Version.Build + 1);
+            Log.Information("Shared library version: {SharedLibVersion}", SharedLibVersion);
+        });
+
+    Target Shared_Build => _ => _
+        .DependsOn(Solution_Restore, Shared_FindNextVersion)
+        .Executes(() =>
+        {
+            DotNetBuild(x => x
+                .SetProjectFile(Solution.Projects.First(x => x.Name.Equals("RestApia.Shared", StringComparison.Ordinal)))
+                .SetConfiguration(Configuration)
+                .SetVersion(SharedLibVersion)
+                .SetOutputDirectory(SharedDirectory));
+        });
+
+    Target Shared_Push => _ => _
+        .DependsOn(Shared_Build)
+        .Executes(() =>
+        {
+            var path = Directory.GetFiles(SharedDirectory, "*.nupkg", SearchOption.AllDirectories).FirstOrDefault();
+            if (string.IsNullOrWhiteSpace(path)) throw new FileNotFoundException("Shared library wasn't found.");
+
+            DotNetNuGetPush(x => x
+                .SetTargetPath(path)
+                .SetSource("https://api.nuget.org/v3/index.json")
+                .SetApiKey(PushApiKey));
+        });
+}
---
+++ .build/Targets/Solution_Targets.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Nuke.Common;
+using Nuke.Common.Tools.DotNet;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+partial class Build
+{
+    Target Solution_Clean => _ => _
+        .Executes(() =>
+        {
+            if (Directory.Exists(OutputDirectory)) Directory.Delete(OutputDirectory, true);
+            DotNetClean(x => x.SetProject(Solution));
+        });
+
+    Target Solution_Restore => _ => _
+        .After(Solution_Clean)
+        .Executes(() =>
+        {
+            DotNetRestore(x => x.SetProjectFile(Solution));
+        });
+}
