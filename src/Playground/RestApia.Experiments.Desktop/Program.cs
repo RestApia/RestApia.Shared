c8c62d4da19c2c8f6e28cabe7cfa8cc2877a48d4
Nick Rimmer <NickRimmer@users.noreply.github.com>
2024-11-07 19:19:36 +0100

    cleanup build warnings

--- src/Playground/RestApia.Experiments.Desktop/Program.cs
+++ src/Playground/RestApia.Experiments.Desktop/Program.cs
@@ -4,7 +4,7 @@ using Avalonia.WebView.Desktop;
 
 namespace RestApia.Experiments.Desktop;
 
-sealed class Program
+internal sealed class Program
 {
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
